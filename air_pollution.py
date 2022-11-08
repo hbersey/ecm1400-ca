272604370eca72bbc978b7590b8d0d8dda04d89c
Henry Bersey <henry@bersey.com>
2022-11-08 17:08:29 +0000

    Added air_pollution.py (v2)

---
+++ air_pollution.py
@@ -0,0 +1,60 @@
+import numpy as np
+import typing
+import csv
+
+
+__FILES = [
+    "Pollution-London Harlington.csv",
+    "Pollution-London N Kensington.csv",
+    "Pollution-London Marylebone Road.csv"
+]
+
+NO_DATA = -1.0
+__NO_DATA_TEXT = "No data" # Avoids magic string issues
+
+
+def __ap_dt(date: typing.AnyStr, time: typing.AnyStr):
+    # sort out time format
+    # eg. 24:15:00 becomes 00:15:00
+    if time[:2] == "24":
+        time = f"00{time[2:]}"
+
+    return np.datetime64(f"{date}T{time}")
+
+
+def __ap_float(s: typing.AnyStr):
+    if s == __NO_DATA_TEXT:
+        return NO_DATA
+    return float(s)
+
+
+def __parse_data(f: typing.TextIO):
+    data = []
+    r = csv.reader(f)
+    next(r)  # skip header row
+
+    for (date_s, time_s, no_s, pm10_s, pm25_s) in r:
+        dt = __ap_dt(date_s, time_s)
+        no = __ap_float(no_s)
+        pm10 = __ap_float(pm10_s)
+        pm25 = __ap_float(pm25_s)
+        data.append([dt, no, pm10, pm25])
+
+    return data
+
+
+def load_ap_data():
+    all_data = []
+    for filename in __FILES:
+        path = f"data/{filename}"
+        f = open(path, "r")
+        all_data.append(__parse_data(f))
+        f.close()
+
+    # convert to np array at the end because we don't know the number of rows
+    # (np arrays not opptimised for dynamic-array-like behaviour)
+    return np.array(all_data)
+
+
+if __name__ == "__main__":
+    load_ap_data()
