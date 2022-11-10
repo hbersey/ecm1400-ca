cc026b44c95d2f30bf2e91e440d9720ef59db04f
Henry Bersey <henry@bersey.com>
2022-11-10 00:03:21 +0000

    Added main.__menu() and main.main_menu()

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-# This is a template. 
-# You should modify the functions below to match
-# the signatures determined by the project specification
+import typing
+
+
+def __menu(items: typing.List[typing.Tuple[str, str, typing.Callable]]):
+    fns = {}
+
+    for (option, description, fn) in items:
+        print(f"{option}) {description}")
+        fns[option] = fn
+
+    while True:
+        sel = input("\nYour selection: ").strip()
+        if sel in fns.keys():
+            fns[sel]()
+            break
 
 
 def main_menu():
+    """Your documentation goes here"""
+
+    __menu([
+        ("R", "Access the PR module", reporting_menu),
+        ("I", "Access the MI module", monitoring_menu),
+        ("M", "Access the RM module", intelligence_menu),
+        ("A", "Print the About text", about),
+        ("Q", "Quit the application", quit)
+    ])
+
+
+def reporting_menu():
     """Your documentation goes here"""
     # Your code goes here
 
@@ -20,11 +44,10 @@ def about():
     """Your documentation goes here"""
     # Your code goes here
 
+
 def quit():
     """Your documentation goes here"""
     # Your code goes here
-
-
 
 
 if __name__ == '__main__':
