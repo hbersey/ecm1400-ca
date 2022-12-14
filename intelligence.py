de48bcb2d5b660441ab254a656d927839f6c921c
Henry Bersey <henry@bersey.com>
2022-12-14 17:07:26 +0000

    Added find_red_pixels

--- intelligence.py
+++ intelligence.py
@@ -1,6 +1,21 @@
-def find_red_pixels(*args,**kwargs):
+from skimage.io import imread, imsave
+import numpy.typing as npt
+import numpy as np
+
+def find_red_pixels(map_filename, upper_threshold=100, lower_threshold=50):
     """Your documentation goes here"""
-    # Your code goes here
+    map: npt.NDArray = imread(map_filename)
+    width, height, _ = map.shape
+    im = np.zeros((width, height))
+
+    for y in range(height):
+        for x in range(width): 
+            r, g, b, _ = map[x,y]
+            if r > upper_threshold and g < lower_threshold and b < lower_threshold:
+                im[x, y] = 1
+
+    imsave("map-red-pixels.jpg", im)
+
 
 def find_cyan_pixels(*args,**kwargs):
     """Your documentation goes here"""
