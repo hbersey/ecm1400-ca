6fd2033d81355df2f9b540f19f1aee5587fa791d
Henry Bersey <henry@bersey.com>
2022-12-16 15:45:27 +0000

    Added test for reporting.count_missing_data

---
+++ test/reporting_count_missing_data_test.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from air_pollution import NO_DATA
+from reporting import count_missing_data
+import pytest
+
+__DATA = {
+    "TEST": pd.DataFrame([
+        [pd.Timestamp("2022-11-23T00:00"), 10.0, 10.0, NO_DATA],
+        [pd.Timestamp("2022-11-23T00:00"), 10.0, NO_DATA, NO_DATA],
+        [pd.Timestamp("2022-11-23T00:00"), 10.0, 10.0, NO_DATA],
+        [pd.Timestamp("2022-11-23T00:00"), 10.0, 10.0, NO_DATA],
+    ], columns=["dt", "no", "pm10", "pm25"])
+}
+
+
+def test_count_missing_data():
+    # Normal Case
+    assert count_missing_data(__DATA, "TEST", "no") == 0
+    assert count_missing_data(__DATA, "TEST", "pm10") == 1
+    assert count_missing_data(__DATA, "TEST", "pm25") == 4
+
+    # Invalid Station + Pollutant
+    with pytest.raises(KeyError):
+        count_missing_data(__DATA, "ABC", "pm100")
+
+    with pytest.raises(KeyError):
+        count_missing_data(__DATA, "TEST", "pm100")
