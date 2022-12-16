32aabcb7b3c43c8490b58e58b162dc04f9381ee4
Henry Bersey <henry@bersey.com>
2022-12-16 15:54:28 +0000

    Fixed test for __ap_float

--- test/air_pollution_ap_float_test.py
+++ test/air_pollution_ap_float_test.py
@@ -1,7 +1,7 @@
-from air_pollution import __ap_float, NO_DATA
+from air_pollution import __ap_float, NO_DATA_TEXT
 
 
 def test___ap_float():
     assert __ap_float("1.123") == 1.123
     assert __ap_float("-1") == -1.0
-    assert __ap_float("No data") == NO_DATA
+    assert __ap_float("No data") == NO_DATA_TEXT
