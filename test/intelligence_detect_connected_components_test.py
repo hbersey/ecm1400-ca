91b604c34af55468a15d50fdbee832bbd2efa6d7
Henry Bersey <henry@bersey.com>
2022-12-18 23:16:42 +0000

    Added intelligence_detect_connected_components_test.py

---
+++ test/intelligence_detect_connected_components_test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import intelligence as mi
+
+__IMG = np.array([
+    [255, 0, 0, 0, 0 ],
+    [255, 255, 0, 0, 0 ],
+    [0, 0, 0, 255, 255 ],
+    [0, 0, 0, 255, 255 ],
+    [0, 0, 0, 0, 0 ]
+])
+
+__MARK = np.array([
+    [1, 0, 0, 0, 0 ],
+    [1, 1, 0, 0, 0 ],
+    [0, 0, 0, 1, 1 ],
+    [0, 0, 0, 1, 1 ],
+    [0, 0, 0, 0, 0 ]
+])
+
+def test_detect_connected_components():
+    assert np.array_equal(mi.detect_connected_components(__IMG), __MARK)
+
